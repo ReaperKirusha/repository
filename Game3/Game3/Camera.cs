130fe4056123c865b1394da532e8e40868ecee7c
ReaperKirusha <blizzard.kirusha@gmail.com>
2016-11-08 13:41:18 +0300

    1
    1

---
+++ Game3/Game3/Camera.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game3
+{
+    public class Camera
+    {
+        public Matrix View, Projection;
+
+        protected GraphicsDevice device;
+
+
+        public Camera(GraphicsDevice device)
+        {
+
+            Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver2, device.Viewport.AspectRatio, 0.01f, 100000f);
+            this.device = device;
+            UpdateViewMatrix(new Vector3(0, 0, 0), new Vector3(0, 0, 0), Vector3.Up);
+        }
+
+        public void UpdateViewMatrix(Vector3 position, Vector3 target, Vector3 up)
+        {
+            View = Matrix.CreateLookAt(position, target, up);
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+
+
+    }
+}
